1a36a7f1340e3233be46e8fc306ba843e4dd544d
Joseph Finney <josephfinney@live.com>
2025-02-10 17:00:38 -0600

    Add project files.

---
+++ CSWin32-SelfContained/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Windows.Win32.Foundation;
+using static Windows.Win32.PInvoke;
+
+namespace CSWinRT_SelfContained;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        HWND shellWindow = GetShellWindow();
+        double dpi = 96.0 / GetDpiForWindow(shellWindow);
+        DpiTextblock.Text = $"{dpi} scale factor";
+    }
+}
